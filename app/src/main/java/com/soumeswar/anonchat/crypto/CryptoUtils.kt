1d312054187229383e4b268ce89a1de5ab0f86e1
SOUMESWAR <soumeswarbhuty@gmail.com>
2026-01-01 23:32:57 +0530

    wtf bro

--- app/src/main/java/com/soumeswar/anonchat/crypto/CryptoUtils.kt
+++ app/src/main/java/com/soumeswar/anonchat/crypto/CryptoUtils.kt
@@ -1,5 +1,50 @@
 package com.soumeswar.anonchat.crypto
 
-object CryptoUtils {
+import java.security.PrivateKey
+import java.security.PublicKey
+import javax.crypto.Cipher
+import javax.crypto.KeyAgreement
+import java.security.SecureRandom
+import javax.crypto.spec.GCMParameterSpec
+import javax.crypto.spec.SecretKeySpec
 
+object CryptoUtils {
+    fun deriveSharedSecret(
+        privateKey: PrivateKey,
+        publicKey: PublicKey
+    ) : ByteArray {
+        val keyAgreement = KeyAgreement.getInstance("X25519")
+        keyAgreement.init(privateKey)
+        keyAgreement.doPhase(publicKey, true)
+        return keyAgreement.generateSecret()
+    }
+
+    fun encrypt(key : ByteArray, plaintext : ByteArray) : ByteArray {
+        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+        val secureRandom = SecureRandom()
+        val nonce = ByteArray(12).also {
+            secureRandom.nextBytes(it)
+        }
+        val spec = GCMParameterSpec(128, nonce)
+        cipher.init(
+            Cipher.ENCRYPT_MODE,
+            SecretKeySpec(key, "AES"),
+            spec
+        )
+        val cipherText = cipher.doFinal(plaintext)
+        return nonce + cipherText
+    }
+
+    fun decrypt(key : ByteArray, cipherData : ByteArray) : ByteArray {
+        val nonce = cipherData.copyOfRange(0, 12)
+        val data = cipherData.copyOfRange(12, cipherData.size)
+        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+
+        cipher.init(
+            Cipher.DECRYPT_MODE,
+            SecretKeySpec(key, "AES"),
+            GCMParameterSpec(128, nonce)
+        )
+        return cipher.doFinal(data)
+    }
 }
--- app/src/main/java/com/soumeswar/anonchat/domain/ChatRepository.kt
+++ app/src/main/java/com/soumeswar/anonchat/domain/ChatRepository.kt
@@ -1,18 +1,23 @@
 package com.soumeswar.anonchat.domain
 
+import android.util.Base64
+import com.soumeswar.anonchat.crypto.CryptoUtils
 import com.soumeswar.anonchat.data.Chat
 import com.soumeswar.anonchat.network.OnionClient
 import com.soumeswar.anonchat.network.OnionServer
 import com.soumeswar.anonchat.data.Message
 import com.soumeswar.anonchat.data.Peer
 import java.net.Socket
+import java.security.KeyFactory
 import java.security.KeyPair
+import java.security.spec.X509EncodedKeySpec
 import java.util.concurrent.ConcurrentHashMap
 
 class ChatRepository(
     val onionClient : OnionClient
 ) {
-    private val chats = ConcurrentHashMap<String, Chat>();
+    private val chats = ConcurrentHashMap<String, Chat>()
+    private val sessions = ConcurrentHashMap<String, ByteArray>()
     fun startServer(
         port : Int,
         identity : KeyPair,
@@ -41,12 +46,49 @@ class ChatRepository(
         onUpdate(updated)
     }
 
+    private fun ensureSession(peer : Peer) : ByteArray {
+        return sessions[peer.onionAddress] ?: run {
+            val socket = onionClient.connect(peer.onionAddress)
+            val out = socket.outputStream.bufferedWriter()
+            val input = socket.inputStream.bufferedReader()
+
+            val public = Base64.encodeToString(
+                identity.public.encoded,
+                Base64.NO_WRAP
+            )
+
+            out.write("HELLO:$public\n")
+            out.flush()
+
+            val response = input.readLine()
+
+            val serverPublic = Base64.decode(
+                response.removePrefix("HELLO_ACK:"),
+                Base64.NO_WRAP
+            )
+
+            val shared = CryptoUtils.deriveSharedSecret(
+                identity.private,
+                KeyFactory.getInstance("X25519")
+                    .generatePublic(X509EncodedKeySpec(serverPublic))
+            )
+            sessions[peer.onionAddress] = shared
+            shared
+        }
+    }
+
+    private fun run(function: ChatRepository.() -> Unit): ByteArray {
+
+        return TODO("Provide the return value")
+    }
+
     fun sendMessage(
         peer : Peer,
         text : String,
         onUpdate: (Chat) -> Unit
     )
     {
+        val sessionKey = ensure
         val socket = onionClient.connect(peer.onionAddress)
         val writer = socket.getOutputStream().bufferedWriter()
 
